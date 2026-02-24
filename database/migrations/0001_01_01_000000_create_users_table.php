b0f2d10ba13249ec521934e2853f2c681d2b7c6d
aboutuil41-glitch <aboutuil41@gmail.com>
2026-02-24 12:06:49 +0000

    Migrations
    Added And deployed The Migration

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -17,6 +17,8 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->integer('reputation')->default(0);
+            $table->timestamp('is_banned')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
