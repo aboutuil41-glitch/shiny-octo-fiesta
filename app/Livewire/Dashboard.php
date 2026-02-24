a71b734852576747024bb545648758b774883986
aboutuil41-glitch <aboutuil41@gmail.com>
2026-02-24 23:41:53 +0000

    fixed up the dashboard

--- app/Livewire/Dashboard.php
+++ app/Livewire/Dashboard.php
@@ -2,12 +2,64 @@
 
 namespace App\Livewire;
 
+use App\Models\Accommodation;
+use App\Models\User;
+use Livewire\Attributes\Layout;
 use Livewire\Component;
 
+#[Layout('layouts.app')]
 class Dashboard extends Component
 {
+    public array $stats = [];
+    public string $tab = 'stats';
+    public $users;
+
+    public function mount(): void
+    {
+        $this->statistics();
+        $this->loadUsers();
+    }
+
+    public function statistics(): void
+    {
+        $this->stats = [
+            'Total Users'          => User::whereDoesntHave('roles', fn($q) => $q->where('name', 'admin'))->whereNull('is_banned')->count(),
+            'Banned Users'         => User::whereNotNull('is_banned')->count(),
+            'Total Accommodations' => Accommodation::where('state', 'active')->count(),
+        ];
+    }
+
+    public function loadUsers(): void
+    {
+        $this->users = User::whereDoesntHave('roles', fn($q) => $q->where('name', 'admin'))->orderBy('created_at', 'desc')->get();
+    }
+
+    public function switchTab(string $tab): void
+    {
+        $this->tab = $tab;
+    }
+
+   public function ban(int $id): void
+{
+    $user = User::find($id);
+    $user->is_banned = now();
+    $user->save();
+    $this->statistics();
+}
+
+public function unban(int $id): void
+{
+    $user = User::find($id);
+    $user->is_banned = null;
+    $user->save();
+    $this->statistics();
+}
+
     public function render()
     {
-        return view('livewire.dashboard');
+        return view('livewire.backoffice.dashboard', [
+            'stats' => $this->stats,
+            'users' => $this->users,
+        ]);
     }
 }
