14308a1113fe322bef2db5e257fb9c6df38be16b
aboutuil41-glitch <aboutuil41@gmail.com>
2026-02-27 14:56:08 +0000

    added a few routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\InvitationController;
+use App\Livewire\Accommondation;
 use App\Livewire\AccommondationForm;
+use App\Livewire\AccommondationList;
 use App\Livewire\Dashboard;
 use App\Livewire\Home;
 use Illuminate\Support\Facades\Route;
@@ -18,13 +21,23 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::middleware('role:admin')->group(function () {
+        Route::get('/admin/dashboard', Dashboard::class)->name('admin.dashboard');
+    });
+
+    Route::get('/home', Home::class)->name('Home');
+    Route::get('/create/room', AccommondationForm::class)->name('add.Accommondation');
+    Route::get('/list', AccommondationList::class)->name('view.Accommondations');
+    Route::get('/view/{id}', Accommondation::class)->name('view.Accommondation');
+
+    Route::get('/invitation/{token}', [InvitationController::class, 'show'])->name('invitation.show');
+    Route::post('/invitation/{token}/accept', [InvitationController::class, 'accept'])->name('invitation.accept');
+    Route::post('/invitation/{token}/decline', [InvitationController::class, 'decline'])->name('invitation.decline');
+    Route::get('/404', function(){
+        return view('errors.404', ['exception' => new \Symfony\Component\HttpKernel\Exception\NotFoundHttpException('you get one BOY!')]);
+    })->name('404-page');
+    
 });
 
-Route::middleware('auth')->group(function () {
-Route::middleware('role:admin')->group(function () {
-        Route::get('/admin/dashboard', Dashboard::class)->name('admin.dashboard');
-});
-        Route::get('/home', Home::class)->name('Home');
-        Route::get('/create/room', AccommondationForm::class)->name('add.Accommondation');
-});
 require __DIR__.'/auth.php';
